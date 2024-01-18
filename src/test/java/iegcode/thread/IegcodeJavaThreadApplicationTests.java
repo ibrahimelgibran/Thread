07e3eebd081de446a985765cb5a4775ee03839cf
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-01-18 12:45:51 +0700

    first commit

---
+++ src/test/java/iegcode/thread/IegcodeJavaThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package iegcode.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IegcodeJavaThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
